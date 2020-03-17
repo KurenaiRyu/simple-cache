a1dc8e7e492432c3cce90f28b302e912eefd28b8
liufuhong <kurenai233@yahoo.com>
2020-03-17 14:24:45 +0800

    fix(*): 修改缓存工厂

--- simple-cache-micronaut-starter/src/main/java/io/github/natsusai/cache/micronaut/starter/RedisCacheFactory.java
+++ simple-cache-micronaut-starter/src/main/java/io/github/natsusai/cache/micronaut/starter/RedisCacheFactory.java
@@ -1,19 +1,18 @@
 package io.github.natsusai.cache.micronaut.starter;
 
 import io.github.natsusai.cache.core.Cache;
+import io.github.natsusai.cache.core.redis.jedis.JedisCache;
+import io.github.natsusai.cache.core.redis.jedis.ShardedJedisCache;
 import io.github.natsusai.cache.core.redis.lettuce.LettuceCache;
 import io.github.natsusai.cache.core.redis.lettuce.LettuceClusterCache;
 import io.lettuce.core.RedisClient;
-import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.cluster.RedisClusterClient;
-import io.lettuce.core.cluster.api.sync.RedisClusterCommands;
-import io.micronaut.configuration.lettuce.DefaultRedisClientFactory;
-import io.micronaut.configuration.lettuce.NamedRedisClientFactory;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.annotation.Value;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.ShardedJedisPool;
 
-import javax.inject.Named;
 import javax.inject.Singleton;
 
 /**
@@ -27,18 +26,28 @@ public class RedisCacheFactory {
   @Value("micronaut.application.name")
   private String appName;
 
-  @Requires(beans = DefaultRedisClientFactory.class)
-  @Singleton()
-  @Named("cache")
-  public Cache<RedisCommands<String, Object>> lettuce(RedisClient redisClient) {
+  @Singleton
+  @Requires(beans = RedisClient.class)
+  public Cache lettuceCache(RedisClient redisClient) {
     return new LettuceCache(appName, redisClient);
   }
 
-  @Requires(beans = NamedRedisClientFactory.class)
-  @Singleton()
-  @Named("cache")
-  public Cache<RedisClusterCommands<String, Object>> lettuce(RedisClusterClient redisClient) {
+  @Singleton
+  @Requires(beans = RedisClusterClient.class)
+  public Cache lettuceClusterCache(RedisClusterClient redisClient) {
     return new LettuceClusterCache(appName, redisClient);
+  }
+
+  @Singleton
+  @Requires(beans = JedisPool.class)
+  public Cache jedisCache(JedisPool jedisPool) {
+    return new JedisCache(appName, jedisPool);
+  }
+
+  @Singleton
+  @Requires(beans = ShardedJedisPool.class)
+  public Cache shardedJedisCache(ShardedJedisPool shardedJedisPool) {
+    return new ShardedJedisCache(appName, shardedJedisPool);
   }
 
 }
